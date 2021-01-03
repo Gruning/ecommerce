972f81be64477966ca8db3222376b1cc9f97d3d3
AlbertoGruning <alberto.gruning.zen@gmail.com>
2021-01-03 13:38:39 -0300

    cap9-firstController

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,7 +1,19 @@
+using Microsoft.AspNetCore.Mvc
+
 namespace API.Controllers
 {
-    public class ProductsController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController: ControllerBase
     {
-        
+
+        [HttpGet]
+        public string GetProducts(){
+          return "this will be a list of products";
+        }
+        [HttpGet("{id}")]
+        public string GetProduct(int id){
+          return "single product";
+        }
     }
 }
